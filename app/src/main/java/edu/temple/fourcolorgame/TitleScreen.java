90ecdb47032f6570ee8df6686e48a659e8dd11a2
Ben <tue94788@temple.edu>
2016-10-07 12:51:15 -0400

    Initial directory setup

---
+++ app/src/main/java/edu/temple/fourcolorgame/TitleScreen.java
@@ -0,0 +1,14 @@
+package edu.temple.fourcolorgame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TitleScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+        String change;
+    }
+}
