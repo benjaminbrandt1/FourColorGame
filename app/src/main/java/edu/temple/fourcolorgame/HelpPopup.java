0c734185c163be66aa8a9f0e80f5a8e27cb1341a
Ben <tue94788@temple.edu>
2016-10-28 12:50:15 -0400

    altered to handle help messages for any activity

--- app/src/main/java/edu/temple/fourcolorgame/HelpPopup.java
+++ app/src/main/java/edu/temple/fourcolorgame/HelpPopup.java
@@ -1,17 +1,27 @@
 package edu.temple.fourcolorgame;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.TextView;
 
 public class HelpPopup extends AppCompatActivity {
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_popup);
+
+        Intent intent = getIntent();
+        int textID = Integer.parseInt(intent.getStringExtra(HelpButtonListener.helpText));
+
+        ((TextView)findViewById(R.id.help_text)).setText(textID);
+
+
 
         double hMultiplier = 1;
 
@@ -38,4 +48,6 @@ public class HelpPopup extends AppCompatActivity {
             }
         });
     }
+
+
 }
